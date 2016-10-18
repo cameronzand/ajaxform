d0e7a50082a04e08a8f6b50afd63caea12476957
Cameron Zandkarimi <camcamcz@gmail.com>
2016-10-18 13:45:15 -0400

    final commit

--- ajaxform/src/js/main.js
+++ ajaxform/src/js/main.js
@@ -34,16 +34,45 @@ function formTemplate (index, array) {
 
 	if (array[index].type === "select"){
 		var HTML = `
-		<select class="selectBar" name="selectB">`;
-		console.log(array[index].options);
+		<select class="selectBar" name="selectB">
+		<option>Select language...</option>`;
 		for (var i=0; i<array[index].options.length; i++){
-			var optionHTML = `<option>${array[index].options[i].label}</option>`;
+			var optionHTML = `
+
+
+			<option>${array[index].options[i].label}</option>`;
 			HTML = HTML+optionHTML;
-			console.log("test");
 		}
 		HTML=HTML+`</select>`;
 		return HTML;
-	} else {
+	} 
+
+
+ else if (array[index].label === "Your Comment"){
+		var HTML = `
+		
+		<i class="fa ${array[index].icon}  kewl"></i>
+		<textarea class="yourComment test" type="${array[index].type}" placeholder="${array[index].label}"></textarea>`;
+
+		return HTML;
+}
+
+
+
+
+
+ else if (array[index].label === "Mobil Number"){
+		var HTML = `
+		
+		<i class="fa ${array[index].icon}  mobil"></i>
+		<input class="inputBar" type="${array[index].type}" placeholder="${array[index].label}">`;
+
+		return HTML;
+}
+
+
+
+	else {
 		var HTML = `
 		
 		<i class="fa ${array[index].icon}"></i>
